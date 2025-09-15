83a48fb964c24ee0e4299cee8fd37e476253201c
Zekai Wang <wangzeka@shernet.sheridancollege.ca>
2025-09-15 15:33:46 -0400

    Finished task 1 where the player drops a bomb on an inoffset after pressing b

---
+++ Assets/Scripts/Journal 2/Controllers/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public Transform plrTransform; 
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+
+    public Vector3 bombOffset = new Vector3(0, 1, 0);
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+
+            SpawnBombAtOffset(bombOffset);
+
+        }
+
+    }
+
+    private void SpawnBombAtOffset(Vector3 offset)
+    {
+
+        Instantiate(bombPrefab, plrTransform.position + offset, Quaternion.identity);
+
+    }
+
+}
